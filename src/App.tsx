760336c914edb149ad1f40f38cc0c6c617554dc2
Sjpulfer <sjpulfer18@hotmail.co.uk>
2025-11-05 06:31:27 +0000

    cleaned up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { BuilderComponent, builder } from '@builder.io/react';
+import './App.css';
+
+builder.init(import.meta.env.VITE_BUILDER_PUBLIC_API_KEY);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    builder
+      .get('page', { url: window.location.pathname })
+      .promise()
+      .then(setContent);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {content ? (
+        <BuilderComponent model="page" content={content} />
+      ) : (
+        <h2>No Builder.io content found for this route</h2>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
